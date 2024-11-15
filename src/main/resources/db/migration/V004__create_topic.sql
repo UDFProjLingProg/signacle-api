146f43179a3f60d05897bc6f24443797ae673056
Paulo Abdiel <abdielpaulo2@gmail.com>
2024-11-15 16:45:34 -0300

    fix: update migrations

--- src/main/resources/db/migration/V004__create_topic.sql
+++ src/main/resources/db/migration/V004__create_topic.sql
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS libras.tb_topics
     created_date    TIMESTAMP NOT NULL
 ); 
 
-COMMENT ON TABLE libras.tb_topicos IS 'Tabela que armazena informações sobre os topicos.';
+COMMENT ON TABLE libras.tb_topics IS 'Tabela que armazena informações sobre os topicos.';
 
-COMMENT ON COLUMN libras.tb_topicos.id_topic IS 'Identificador único do tópico.';
-COMMENT ON COLUMN libras.tb_topicos.word IS 'Palavra do tópivo.';
-COMMENT ON COLUMN libras.tb_topicos.description IS 'Descrição da palavra.';
-COMMENT ON COLUMN libras.tb_topicos.image IS 'Image do tópico.';
-COMMENT ON COLUMN libras.tb_topicos.comment IS 'Comentário do Tópico';
-COMMENT ON COLUMN libras.tb_topicos.created_date IS 'Data de criação do registro do usuário.';
+COMMENT ON COLUMN libras.tb_topics.id_topic IS 'Identificador único do tópico.';
+COMMENT ON COLUMN libras.tb_topics.word IS 'Palavra do tópivo.';
+COMMENT ON COLUMN libras.tb_topics.description IS 'Descrição da palavra.';
+COMMENT ON COLUMN libras.tb_topics.image IS 'Image do tópico.';
+COMMENT ON COLUMN libras.tb_topics.comment IS 'Comentário do Tópico';
+COMMENT ON COLUMN libras.tb_topics.created_date IS 'Data de criação do registro do usuário.';
