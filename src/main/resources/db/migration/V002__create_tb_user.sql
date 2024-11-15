bff503f0a1eab4acd494619b3206e685741ba98f
Paulo Abdiel <65676235+Paulo-dev2@users.noreply.github.com>
2024-11-15 19:39:46 -0300

    Features (#10)
    * Chore: arrumando FlyWay
    
    * Feat: adicionando configuração jwt/registrar usuário/configuração de email
    
    * Develop (#6) (#7)
    
    * Chore: arrumando FlyWay
    
    * Feat: adicionando configuração jwt/registrar usuário/configuração de email
    
    * fix: tabela topicos
    
    * fix: update migrations
    
    * fix: update table tb_users
    
    * feat: add table roles
    
    ---------
    
    Co-authored-by: ArturMartins <arturdantasmartins@gmail.com>
    Co-authored-by: ArturMartins <54951997+MartinsArtur@users.noreply.github.com>

--- src/main/resources/db/migration/V002__create_tb_user.sql
+++ src/main/resources/db/migration/V002__create_tb_user.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS libras.tb_users
     last_name       VARCHAR,
     email           VARCHAR,
     password        VARCHAR,
+    account_locked  BOOLEAN,
+    enabled         BOOLEAN,
     created_date    TIMESTAMP NOT NULL
 );
 
@@ -16,5 +18,7 @@ COMMENT ON COLUMN libras.tb_users.id_user IS 'Identificador único do usuário.'
 COMMENT ON COLUMN libras.tb_users.first_name IS 'Primeiro nome do usuário.';
 COMMENT ON COLUMN libras.tb_users.last_name IS 'Sobrenome do usuário.';
 COMMENT ON COLUMN libras.tb_users.email IS 'Endereço de email do usuário.';
+COMMENT ON COLUMN libras.tb_users.account_locked IS 'Verificação se a conta do usuário está blockeada.';
+COMMENT ON COLUMN libras.tb_users.enabled IS 'Verificação se a conta do usuário está ativada.';
 COMMENT ON COLUMN libras.tb_users.password IS 'Senha de acesso do usuário (armazenada de forma segura, em hash).';
 COMMENT ON COLUMN libras.tb_users.created_date IS 'Data de criação do registro do usuário.';
