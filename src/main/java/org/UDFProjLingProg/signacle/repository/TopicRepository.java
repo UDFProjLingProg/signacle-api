b5f0919d4671d0d370754bbaad33dfc2e02ec6f9
Paulo Abdiel <abdielpaulo2@gmail.com>
2024-11-16 17:52:14 -0300

    feat: create repository Topic

---
+++ src/main/java/org/UDFProjLingProg/signacle/repository/TopicRepository.java
@@ -0,0 +1,13 @@
+package org.UDFProjLingProg.signacle.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.UDFProjLingProg.signacle.entities.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TopicRepository extends JpaRepository<Topic, UUID> {
+    Optional<Topic> findByWord(String word);
+}
